5853e692ac95e88442e6fb2fc40c7c4b2026fe7e
Gabriel Donada <gabriel.donada@sap.com>
2023-09-29 00:04:46 -0300

    Adding some routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,12 +3,15 @@ import { AbstractUserRepository } from './repositories/interfaces/abstract-user-
 import { AuthenticationUserBody } from './dtos/authentication-body';
 import { AbstractVehicle } from './repositories/interfaces/abstract-vehicle-repository';
 import { VehicleType } from './dtos/vehicle-type';
+import { AbstractMaintenance } from './repositories/interfaces/abstract-maintenance';
+import { MaintenanceType } from './dtos/maintenance-type';
 
 @Controller('api')
 export class AppController {
   constructor(
     private createNewUserRepository: AbstractUserRepository,
-    private vehicle: AbstractVehicle
+    private vehicle: AbstractVehicle,
+    private maintenance: AbstractMaintenance
     ) {}
     
 
@@ -41,4 +44,49 @@ export class AppController {
     await this.vehicle.archiveVehicle(body.id);
   }
 
+  @Get('maintenanceByVehicle')
+  async getMaintenanceByVehicle(@Body() body: VehicleType){
+    await this.maintenance.getMaintenanceByVehicle(body.id);
+  }
+
+  @Get('getMaintenanceAllVehicle')
+  async getMaintenanceAllVehicle(){
+    await this.maintenance.getMaintenanceAllVehicle();
+  }
+
+  @Get('registerMaintenance')
+  async registerMaintenance(@Body() body: MaintenanceType){
+    await this.maintenance.registerMaintenance(
+      body.date,
+      body.mainType,
+      body.vehKm,
+      body.totalAmout, 
+      body.vehicleId
+    );
+  }
+
+  @Get('getMaintenceTotals')
+  async getMaintenceTotals(){
+    await this.maintenance.getMaintenceTotals();
+  }
+
+  @Get('getMaintenceByPeriod')
+  async getMaintenceByPeriod(@Body() body: string){
+    await this.maintenance.getMaintenceByPeriod(body);
+  }
+
+  @Put('updateVehicleKm')
+  async updateVehicleKm(@Body() body:{vehicleId: string, newKm: number}){
+    await this.vehicle.updateVehicleKm(body.vehicleId, body.newKm)
+  }
+
+  @Get('getPreventiveMaintenance')
+  async getPreventiveMaintenance(){
+    await this.vehicle.getPreventiveMaintenance();
+  }
+
+  @Get('getNeedMaintenance')
+  async getNeedMaintenance(){
+    await this.vehicle.getNeedMaintenance();
+  }
 }
--- src/dtos/maintenance-type.ts
+++ src/dtos/maintenance-type.ts
@@ -4,4 +4,5 @@ export class MaintenanceType {
     mainType: string;
     vehKm: number;
     totalAmout: number;
+    vehicleId: string;
 }
